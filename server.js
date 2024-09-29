70c33f45ae9c444da7923c1bb166ee9ee30f3f68
MRBossASIF-DEV <ccloudcode@gmail.com>
2024-09-29 08:49:51 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        // Broadcast to all connected clients
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Signaling server is running on ws://localhost:3000');
+});
